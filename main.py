854af0a95b87f4f317981dc4afd87f36d7e39794
Sasha <2323@vntu.edu.ua>
2024-08-27 09:29:56 +0300

    modified main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ if not randint(0, 5):
 	print("You are lucky aye!")
 else:
 	print("Farewell World!")
+
+if not randint(0, 9):
+	print("You are lucky again aye!")
+else:
+	print("But not this time!")
